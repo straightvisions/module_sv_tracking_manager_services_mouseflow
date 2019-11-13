01b564631cb14d3ee0b211ee7634ae719dfd5549
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-11-13 15:31:43 +0100

    init

---
+++ mouseflow.php
@@ -0,0 +1,79 @@
+<?php
+	namespace sv_tracking_manager;
+	
+	/**
+	 * @version         1.000
+	 * @author			straightvisions GmbH
+	 * @package			sv100
+	 * @copyright		2019 straightvisions GmbH
+	 * @link			https://straightvisions.com
+	 * @since			1.000
+	 * @license			See license.txt or https://straightvisions.com
+	 */
+	
+	class mouseflow extends modules {
+		public function init() {
+			// Section Info
+			$this->set_section_title( __('Mouseflow', 'sv_tracking_manager' ) )
+				 ->set_section_desc(__( sprintf('%sMouseflow Login%s', '<a target="_blank" href="https://app.mouseflow.com/sign-in">','</a>'), 'sv_tracking_manager' ))
+				 ->set_section_type( 'settings' )
+				 ->load_settings()
+				 ->register_scripts()
+				 ->get_root()->add_section( $this );
+			
+			add_action('init', array($this, 'load'));
+		}
+		
+		protected function load_settings(): mouseflow {
+			$this->get_setting('activate')
+				 ->set_title( __( 'Activate', 'sv_tracking_manager' ) )
+				 ->set_description('Enable Tracking')
+				 ->load_type( 'checkbox' );
+			
+			$this->get_setting('tracking_id')
+				 ->set_title( __( 'Tracking ID', 'sv_tracking_manager' ) )
+				 ->set_description( __( sprintf('%sHow to retrieve Tracking ID%s', '<a target="_blank" href="https://support.mouseflow.com/support/solutions/articles/44001578383-how-do-i-find-my-mouseflow-site-id-">','</a>'), 'sv_tracking_manager' ) )
+				 ->load_type( 'text' );
+			return $this;
+		}
+		protected function register_scripts(): mouseflow {
+			if($this->is_active()) {
+				$this->get_script('default')
+					 ->set_path('lib/frontend/js/default.js')
+					 ->set_type('js');
+			}
+			
+			return $this;
+		}
+		public function is_active(): bool{
+			// activate not set
+			if(!$this->get_setting('activate')->run_type()->get_data()){
+				return false;
+			}
+			// activate not true
+			if($this->get_setting('activate')->run_type()->get_data() !== '1'){
+				return false;
+			}
+			// Tracking ID not set
+			if(!$this->get_setting('tracking_id')->run_type()->get_data()){
+				return false;
+			}
+			// Tracking ID empty
+			if(strlen(trim($this->get_setting('tracking_id')->run_type()->get_data())) === 0){
+				return false;
+			}
+			
+			return true;
+		}
+		public function load(): mouseflow{
+			if($this->is_active()){
+				$this->get_script('default')
+					 ->set_is_enqueued()
+					 ->set_localized(array(
+						 'tracking_id'	=> $this->get_setting('tracking_id')->run_type()->get_data()
+					 ));
+			}
+			
+			return $this;
+		}
+	}
